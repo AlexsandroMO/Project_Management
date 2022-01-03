8b7b6d310584db3a121849dfbd0a7a68cfedb3c2
AlexsandroMO <sandrobass@hotmail.com>
2022-01-03 20:21:38 -0300

    Reset All

--- documentation/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-#http://opensource.locaweb.com.br/locawebstyle-v2/manual/componentes/tabelas/
-
-##{% url 'admin:index' %}documentation/myproject/{{proj.id}}/change/
-
