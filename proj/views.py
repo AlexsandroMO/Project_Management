17666bcbf4febeb27ee1d8e98900f7d36c632180
AlexsandroMO <sandrobass@hotmail.com>
2022-01-09 20:18:27 -0300

    update

--- proj/views.py
+++ proj/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import MyProject, DocumentBase #, PageT, DocT, DocumentStandard, Subject, Action, StatusDoc, Employee, Cotation, Upload, ProjectValue, LdProj
+from .models import MyProject, Subject, DocumentBase, DocumentListProject #, PageT, DocT, DocumentStandard, Subject, Action, StatusDoc, Employee, Cotation, Upload, ProjectValue, LdProj
 #from .forms import MyProjectForm, SubjectForm, PageTForm, DocTForm, PageformatForm, DocumentStandardForm, EmployeeForm, StatusDocForm, ActionForm, CotationForm, LdProjForm
 from datetime import datetime
 
@@ -39,17 +39,24 @@ def OnProjects(request):
 
 
 @login_required
-def DocumentList(request):
+def BaseDoc(request):
 
-  return render(request, 'proj/document-list.html') #, {'MyProjects': MyProjects,'proj':proj, 'Employees':Employees, 'colaborador':colaborador, 'photo_colab':photo_colab})
+  Subjects = Subject.objects.all().order_by('-subject_name')
+  DocumentBases = DocumentBase.objects.all().order_by('-document_name')
+  return render(request, 'proj/document-base.html',{'DocumentBases':DocumentBases, 'Subjects':Subjects}) #, {'MyProjects': MyProjects,'proj':proj, 'Employees':Employees, 'colaborador':colaborador, 'photo_colab':photo_colab})
 
 
 @login_required
-def BaseDoc(request):
+def DocumentList(request):
+  POST = dict(request.POST)
+  POST2 = dict(request.GET)
 
-  MyProjects = MyProject.objects.all().order_by('-project_name')
-  DocumentBases = DocumentBase.objects.all().order_by('-document_name')
-  return render(request, 'proj/document-base.html',{'DocumentBases':DocumentBases, 'MyProjects':MyProjects}) #, {'MyProjects': MyProjects,'proj':proj, 'Employees':Employees, 'colaborador':colaborador, 'photo_colab':photo_colab})
+  print('>>>>', POST)
+  print('>>ID >>', POST2)
+
+  ListDoc = DocumentListProject.objects.all().order_by('-doc_name_pattern')
+
+  return render(request, 'proj/document-list.html',{'ListDoc':ListDoc}) #, {'MyProjects': MyProjects,'proj':proj, 'Employees':Employees, 'colaborador':colaborador, 'photo_colab':photo_colab})
 
 
 @login_required
